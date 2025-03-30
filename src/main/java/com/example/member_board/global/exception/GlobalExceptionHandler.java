4aa006e11d93f0d6ee71003a55472d922df19927
limjunseo <limjunseo123123@gmail.com>
2025-03-30 21:03:06 +0900

    [feat] 공통에러 핸들러

---
+++ src/main/java/com/example/member_board/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.example.member_board.global.exception;
+
+import com.example.member_board.global.exception.customException.user.UserException;
+import com.example.member_board.global.exception.errorCode.CommonErrorCode;
+import com.example.member_board.global.response.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestControllerAdvice.class)
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = {UserException.class})
+    public ResponseEntity<Object> handleUserException(UserException e) {
+        CommonErrorCode errorCode = e.getErrorCode();
+
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(ErrorResponse.from(errorCode));
+    }
+
+}
