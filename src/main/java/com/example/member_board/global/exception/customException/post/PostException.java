1bb71fbc781faa32b805cf0754e5208e5c38ed5d
limjunseo <limjunseo123123@gmail.com>
2025-03-30 21:03:51 +0900

    [feat] 사용자 예외

---
+++ src/main/java/com/example/member_board/global/exception/customException/post/PostException.java
@@ -0,0 +1,14 @@
+package com.example.member_board.global.exception.customException.post;
+
+import com.example.member_board.global.exception.errorCode.CommonErrorCode;
+import lombok.Getter;
+
+@Getter
+public class PostException extends RuntimeException {
+    private final CommonErrorCode commonErrorCode;
+
+    public PostException(CommonErrorCode commonErrorCode) {
+        this.commonErrorCode = commonErrorCode;
+    }
+
+}
---
+++ src/main/java/com/example/member_board/global/exception/customException/user/UserException.java
@@ -0,0 +1,16 @@
+package com.example.member_board.global.exception.customException.user;
+
+import com.example.member_board.global.exception.errorCode.CommonErrorCode;
+import lombok.Getter;
+
+@Getter
+//유저 관련 예외처리
+public class UserException extends RuntimeException {
+    private final CommonErrorCode errorCode;
+
+    //errorCode 기본 메세지
+    public UserException(CommonErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+}
